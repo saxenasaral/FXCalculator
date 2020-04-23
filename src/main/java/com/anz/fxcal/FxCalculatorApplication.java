c46a3de7b9b34a113737180f0dfa60211398c262
vartika saxena <saxenasaral0@gmail.com>
2020-04-23 16:42:47 +0530

    fresh

---
+++ src/main/java/com/anz/fxcal/FxCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.anz.fxcal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anz/fxcal/FxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anz.fxcal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
